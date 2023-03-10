f2516eee253d98b3a0def1f501a7270eb9c7dd23
Valeria <valeriaelisabetrgz@gmail.com>
2023-03-10 11:03:49 -0300

    add firebase

---
+++ JS/app.js
@@ -0,0 +1,36 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js";
+
+var firebaseConfig = {
+    apiKey: "AIzaSyAW_vJQ1HAlg7FoQH6nzbji8AgHEFv_68U",
+    authDomain: "guitarapi-b6d03.firebaseapp.com",
+    databaseURL: "https://guitarapi-b6d03-default-rtdb.firebaseio.com",
+    projectId: "guitarapi-b6d03",
+    storageBucket: "guitarapi-b6d03.appspot.com",
+    messagingSenderId: "470660630915",
+    appId: "1:470660630915:web:0cb91d8d3083233d488eb6"
+  };
+
+var app = initializeApp(firebaseConfig);
+const openModal = document.getElementById('openRegisterModal')
+const modal = document.getElementById('modal')
+const closeModal = document.getElementById('cancelRegistreModal')
+const registerForm = document.getElementById('registerForm')
+const guitarRef = app.database().ref('guitars')
+
+const showRegisterModal = () => {
+    modal.classList.toggle('is-active')
+};
+openModal.addEventListener('click', showRegisterModal)
+closeModal.addEventListener('click', showRegisterModal)
+registerForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const nombre = registerForm['nombre'].value
+    //Agregar los inputs restantes
+
+    const registerGuitar = guitarRef.push()
+    registerGuitar.set({
+        Uid: registerGuitar.path.pieces_[1],
+        Nombre: nombre 
+    })
+    showRegisterModal()
+})
